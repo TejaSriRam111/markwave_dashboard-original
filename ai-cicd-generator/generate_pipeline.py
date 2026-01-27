1e46fb5c9b29972d52183bba94bde56f46129bf5
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-27 11:41:46 +0530

    Update generate_pipeline.py

--- ai-cicd-generator/generate_pipeline.py
+++ ai-cicd-generator/generate_pipeline.py
@@ -1,15 +1,17 @@
 import shutil
+import os
+import sys
 
-def generate(stack):
-    if stack == "node":
-        shutil.copy("templates/node.yml", ".github/workflows/ci.yml")
-    elif stack == "python":
-        shutil.copy("templates/python.yml", ".github/workflows/ci.yml")
-    else:
-        raise Exception("Unsupported stack")
+stack = sys.argv[1]
 
-if __name__ == "__main__":
-    import os
-    stack = os.environ.get("STACK")
-    os.makedirs(".github/workflows", exist_ok=True)
-    generate(stack)
+os.makedirs(".github/workflows", exist_ok=True)
+
+if stack == "markwave_react":
+    shutil.copy(
+        "templates/markwave_react.yml",
+        ".github/workflows/ci.yml"
+    )
+else:
+    raise Exception("Unsupported repository type")
+
+print("CI pipeline generated")
