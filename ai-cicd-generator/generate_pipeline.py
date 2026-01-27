84d7976949bc61eda16a87126bf6750c493b4297
BILLURI TEJA SRI RAM <164980774+TejaSriRam111@users.noreply.github.com>
2026-01-27 17:11:43 +0530

    Update generate_pipeline.py

--- ai-cicd-generator/generate_pipeline.py
+++ ai-cicd-generator/generate_pipeline.py
@@ -4,14 +4,17 @@ import sys
 
 stack = sys.argv[1]
 
-os.makedirs(".github/workflows", exist_ok=True)
+REPO_ROOT = os.path.abspath(os.path.join(os.getcwd(), ".."))
+WORKFLOW_DIR = os.path.join(REPO_ROOT, ".github", "workflows")
+
+os.makedirs(WORKFLOW_DIR, exist_ok=True)
 
 if stack == "markwave_react":
     shutil.copy(
         "templates/markwave_react.yml",
-        ".github/workflows/ci.yml"
+        os.path.join(WORKFLOW_DIR, "ci.yml")
     )
 else:
     raise Exception("Unsupported repository type")
 
-print("CI pipeline generated")
+print("CI/CD pipeline generated successfully")
